2d2dd269d91268c96fbc971774dc529075cce425
Aakash sre <aakasre@gmail.com>
2025-08-30 01:42:17 +0530

    first commit

---
+++ src/components/Sidebar/Main/Main.jsx
@@ -0,0 +1,92 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../../assets/assets'
+import { Context } from '../../../context/context'
+
+
+const Main = () => {
+
+  const { onSent, recentPrompt,showResult,loading ,resultData,setInput,input} = useContext(Context)
+
+
+
+  return (
+    <div className='main'>
+      <div className="nav">
+        <p>ShreenKit</p>
+        <img src={assets.newuser_icon} alt="" />
+
+      </div>
+      <div className="main-container">
+
+       {!showResult
+       ? <>
+
+
+        <div className="greet">
+          <p> <span> Hello, Dev</span> </p>
+          <p>How can I Help you today</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful palces to <br/>  see on the beautiful ground</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+           <div className="card">
+            <p>Brifly summerize this  concept : urban plannning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+           <div className="card">
+            <p>Brainstorm team bonding activitise for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+           <div className="card">
+            <p>Improve the readabilty of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+       </>
+       :<div className='result'>
+        <div className='result-title'>
+          <img src={assets.shree_icon} alt="" />
+          <p>{recentPrompt}</p>
+
+        </div>
+        <div className="result-data">
+          <img src={assets.shree_icon} alt="" />
+          {loading 
+          ? <div className='loader' >
+            <hr />
+             <hr />
+              <hr />
+          </div>
+            : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+        }
+        </div>
+
+       </div>
+       }
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input onChange={(e)=> setInput(e.target.value)} value={input} type="text"  placeholder='Enter a prompt here' />
+          <div>
+          <img src={assets.gallery_icon}alt="" />
+           <img src={assets.mic_icon}alt="" />
+            {input? <img onClick={()=> onSent()} src={assets.send_icon}alt="" /> : null}
+        </div> 
+        </div>
+      
+      <p className="bottom-info">
+        Gemini may display inaccurate info including about people , so double check its  
+      </p>
+     </div>
+     </div>
+     </div>
+   
+  
+    
+  )
+}
+
+export default Main
---
+++ src/config/gemini.js
@@ -0,0 +1,118 @@
+
+
+// See https://developers.google.com/apps-script/guides/properties
+// for instructions on how to set the script properties.
+// const apiKey = PropertiesService.getScriptProperties().getProperty('AIzaSyBdrSRzJ_0gol1HHM9PDta-fr2kH9VM7ZE');
+// const model = 'gemini-2.5-pro';
+// const api = 'streamGenerateContent';
+// const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:${api}?key=${apiKey}`;
+
+// function main() {
+//   const payload = {
+//     contents: [
+//       {
+//         role: 'user',
+//         parts: [
+//           {
+//             text: `INSERT_INPUT_HERE`
+//           },
+//         ]
+//       },
+//     ],
+//     generationConfig: {
+//       thinkingConfig: {
+//         thinkingBudget: -1,
+//       },
+//     },
+//     tools: [
+//       {
+//         googleSearch: {}
+//       },
+//     ],
+//   };
+
+//   const options = {
+//     method: 'POST',
+//     contentType: 'application/json',
+//     muteHttpExceptions: true,
+//     payload: JSON.stringify(payload),
+//   };
+
+//   const response = UrlFetchApp.fetch(url, options);
+//   const chunks = JSON.parse(response.getContentText());
+
+//   for (const chunk of chunks) {
+//     console.log(chunk.candidates[0].content.parts[0].text);
+//   }
+// }
+
+// node --version # should be => 18
+//npm install @google/generative-ai
+
+import  {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+
+}  from "@google/generative-ai"
+ 
+const MODEL_NAME = "gemini-2.0-flash";
+const API_KEY = "AIzaSyBdrSRzJ_0gol1HHM9PDta-fr2kH9VM7ZE";
+
+
+async function runChat(prompt) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({model:MODEL_NAME });
+
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK:1,
+    topP: 1,
+    maxOutputTokens:2048,
+
+  };
+
+  const safetySettings= [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold:HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold:HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+
+    },
+    {
+      category:HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold:HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+
+
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history:[
+
+    ],
+
+
+
+
+  });
+
+
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+
+  return response.text();
+
+
+}
+
+ export default runChat;
+
+
